ad350e5c29c37b40f77c3cc7f7355f340c7daa51
Silenthacker804 <121734351+Silenthacker804@users.noreply.github.com>
2023-01-21 23:43:45 +0100

    Add files via upload

--- IG.py
+++ IG.py
@@ -17,7 +17,7 @@ from rich import print as prints
 from rich import pretty
 from rich.text import Text as tekz
 os.system('pip install mechanize requests futures==2 > /dev/null')
-    os.system('python IG.py')
+  
 ###----------[ WARNA PRINT RICH ]---------- ###
 M2 = "[#FF0000]" # MERAH
 H2 = "[#00FF00]" # HIJAU
