84e990d947abadcdb323a395ed9fc1ae7a53cbaa
Silenthacker804 <121734351+Silenthacker804@users.noreply.github.com>
2023-01-27 09:34:56 +0100

    Add files via upload

--- me.py
+++ me.py
@@ -73,6 +73,57 @@ def tahunng(fx):
 	return tahunz
 
 ###---[ANGGAP INI LOGO ]---###
+os.system('clear')
+def chk():
+  uuid = str(os.geteuid()) + str(os.getlogin()) 
+  id = "|".join(uuid)
+  print("\n\n\x1b[37;1m  YOUR ID : \033[94m"+id) 
+  try: 
+    httpCaht = requests.get("https://raw.githubusercontent.com/Silenthacker804/SILENT/main/approve.py").text 
+    if id in httpCaht: 
+      print("\033[92m  YOUR ID IS ACTIVE. .......\033[97m") 
+      msg = str(os.geteuid()) 
+      time.sleep(1) 
+      pass 
+    else: 
+      print("\033[0;93m YOUR ID IS NOT ACTIVE COPY AND SEND ME MESSAGE ON WHATSAPP !!!") 
+      os.system('xdg-open  https://wa.me/2348167676589?text=*Hello*')
+      time.sleep(1) 
+      sys.exit() 
+  except: 
+    sys.exit() 
+    if name == '__main__': 
+     print (logo)
+     chk() 
+    
+chk()
+os.system('clear')
+os.system('clear')
+def chk():
+  uuid = str(os.geteuid()) + str(os.getlogin()) 
+  id = "|".join(uuid)
+  print("\n\n\x1b[37;1m  YOUR ID : \033[94m"+id) 
+  try: 
+    httpCaht = requests.get("https://raw.githubusercontent.com/Silenthacker804/SILENT/main/approve.py").text 
+    if id in httpCaht: 
+      print("\033[92m  YOUR ID IS ACTIVE. .......\033[97m") 
+      msg = str(os.geteuid()) 
+      time.sleep(1) 
+      pass 
+    else: 
+      print("\033[0;93m YOUR ID IS NOT ACTIVE COPY AND SEND ME MESSAGE ON WHATSAPP !!!") 
+      os.system('xdg-open  https://wa.me/2348167676589?text=*Hello*')
+      time.sleep(1) 
+      sys.exit() 
+  except: 
+    sys.exit() 
+    if name == '__main__': 
+     print (logo)
+     chk() 
+    
+chk()
+os.system('clear')
+
 def logo(n):
 	return str(f"""
 \33[31m  ██████╗ ████████╗ ██████╗  ██╗   ██╗
